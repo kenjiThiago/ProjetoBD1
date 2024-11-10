406bb2ba93842d40d54c779bd64f1f1f5bed1867
Thiago Kenji <thiago.kenji123@gmail.com>
2024-11-10 20:01:24 -0300

    [frontend] Setando as rotas do frontend para o vite build.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,3 +1,14 @@
+import { resolve } from "path"
+
 export default {
   root: "src",
+  build: {
+    target: 'esnext',
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "src/index.html"),
+        vagas: resolve(__dirname, "src/vagas/index.html"),
+      },
+    }
+  },
 }
