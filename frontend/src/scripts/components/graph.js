fdbe3f8ce1dba19b100c66122eac19ff83845cb6
Thiago Kenji <thiago.kenji123@gmail.com>
2024-11-11 20:05:21 -0300

    [frontend] Aumentado o gráfico.

--- frontend/src/scripts/components/graph.js
+++ frontend/src/scripts/components/graph.js
@@ -14,6 +14,7 @@ export async function createGraph(num_alunos_ativos, num_alunos_inativos) {
       datasets: [{
         label: "Alunos",
         data: [num_alunos_ativos, num_alunos_inativos],
+        //data: [10000, 10000],
         backgroundColor: [
           "#1D2C45",
           "#E65A00",
@@ -23,22 +24,36 @@ export async function createGraph(num_alunos_ativos, num_alunos_inativos) {
     },
     options: {
       layout: {
-        padding: 40
+        padding: 90
       },
       plugins: {
         legend: {
           labels: {
             color: "#000000",
             font: {
-              size: 14,
+              size: 20,
               weight: 'bold'
             },
+          },
+        },
+        title: {
+          display: true,
+          color: "#222020",
+          text: "Status dos Alunos",
+          font: {
+            size: 30,
+            weight: 'bold'
+          },
+          padding: {
+            top: -12,
+            bottom: 40
           },
         },
         datalabels: {
           color: "#000000",
           font: {
-            size: 14,
+            size: 20,
+            weight: "bold",
           },
           anchor: 'end',
           align: 'end',
@@ -48,7 +63,7 @@ export async function createGraph(num_alunos_ativos, num_alunos_inativos) {
             }, 0)
 
             const percentage = value / totalSum * 100
-            return `${value} (${percentage.toFixed(1)}%)`
+            return `${value}(${percentage.toFixed(1)}%)`
           })
         },
       },
