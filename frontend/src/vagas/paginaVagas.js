ec1a3dbdd384d4d37dd1c465bac1a6233d0b327d
Thiago Kenji <thiago.kenji123@gmail.com>
2024-11-12 19:06:17 -0300

    [frontend] Mudado a localização do tableButtons.js

--- frontend/src/vagas/paginaVagas.js
+++ frontend/src/vagas/paginaVagas.js
@@ -2,7 +2,7 @@ import "../styles/main.css"
 import "../styles/table.css"
 
 import { populateTable } from "./components/tablePaginaVagas.js"
-import { buttonFunctionality } from "./functionalities/tableButtons.js"
+import { buttonFunctionality } from "../scripts/functionalities/tableButtons.js"
 import { inputFunctionality } from "./functionalities/formInput.js"
 import { createHeader } from "../scripts/components/header.js"
 import { createAside } from "../scripts/components/aside.js"
