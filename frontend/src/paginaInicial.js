9c9c92226b06c0763c6352df063c17005c1c0c22
Thiago Kenji <thiago.kenji123@gmail.com>
2024-11-11 21:42:34 -0300

    [frontend] Atulizado o visual do dashboard da páginaInicial.

--- frontend/src/paginaInicial.js
+++ frontend/src/paginaInicial.js
@@ -1,6 +1,11 @@
 import "./styles/main.css"
 import "./styles/paginaInicial.css"
 
+import vagas from "./assets/vagas.svg"
+import cursos from "./assets/cursos.svg"
+import empresas from "./assets/empresas.svg"
+import alunos from "./assets/alunos.svg"
+
 import { createHeader } from "./scripts/components/header.js"
 import { createGraph } from "./scripts/components/graph.js"
 import { createAside } from "./scripts/components/aside.js"
@@ -19,19 +24,31 @@ const cards = document.querySelector("#card-wrapper")
 
 cards.innerHTML = `
 <div class="card">
-  <h2>Número de Vagas</h2>
+  <div class="card-title">
+    <h2>Número de Vagas</h2>
+    <img src="${vagas}" />
+  </div>
   <p><span>${data.num_vagas}</span> Vagas Disponíveis</p>
 </div>
 <div class="card">
-  <h2>Número de Cursos</h2>
+  <div class="card-title">
+    <h2>Número de Cursos</h2>
+    <img src="${cursos}" />
+  </div>
   <p><span>${data.num_cursos}</span> Cursos Disponíveis</p>
 </div>
 <div class="card">
-  <h2>Número de Alunos</h2>
+  <div class="card-title">
+    <h2>Número de Alunos</h2>
+    <img src="${alunos}" />
+  </div>
   <p><span>${data.num_alunos}</span> Alunos Cadastrados</p>
 </div>
 <div class="card">
-  <h2>Número de Empresas</h2>
+  <div class="card-title">
+    <h2>Número de Empresas</h2>
+    <img src="${empresas}" />
+  </div>
   <p><span>${data.num_empresas}</span> Empresas Parceiras</p>
 </div>
 `
