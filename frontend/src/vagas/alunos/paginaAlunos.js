29ab74d319a0684acf91d36162068ab0cfca3165
Thiago Kenji <thiago.kenji123@gmail.com>
2024-11-13 08:15:57 -0300

    [frontend] Arrumado a página Alunos e a tabela.

--- frontend/src/vagas/alunos/paginaAlunos.js
+++ frontend/src/vagas/alunos/paginaAlunos.js
@@ -7,6 +7,7 @@ import { createAside } from "../../scripts/components/aside.js"
 import { asideToggle } from "../../scripts/functionalities/header.js"
 import { buttonFunctionality } from "../../scripts/functionalities/tableButtons.js"
 import { inputFunctionality } from "./functionalities/formInput.js"
+import { populateTable } from "./components/tablePaginaAlunos.js"
 
 const aside = document.querySelector("#app aside")
 
@@ -20,10 +21,10 @@ const response = await fetch("http://localhost:8000/alunos")
 const data = await response.json()
 
 const page = urlParams.get("page")
-const size = data.length
+const size = data.num_ativos + data.num_inativos
 const numberOfPages = Math.ceil(size / 10);
 
-populateTable(page, data, numberOfPages, size)
+populateTable(page, data.alunos, numberOfPages, size)
 
 buttonFunctionality(page, numberOfPages, urlParams)
 
