2762bf72e2dd960ce110ebce03e2157f25762b61
fonsecajohn <joaovictorfonseca@usp.br>
2024-11-12 21:00:29 -0300

    [Backend] colocando a rota dos counts na homepage

--- Backend/main.py
+++ Backend/main.py
@@ -5,13 +5,38 @@ from Rotas.rota_curso import curso_blueprint
 from Rotas.rota_empresa import empresa_blueprint
 from Rotas.rota_professor import professor_blueprint
 from Rotas.rota_vaga import vaga_blueprint
+from Database.classe_aluno import Aluno
+from Database.classe_curso import Curso
+from Database.classe_empresa import Empresa
+from Database.classe_vaga import Vaga
 
 app = Flask(__name__)
 CORS(app, origins="*")
 
 @app.route("/", methods=["GET"])
-def get_autor():
-    return jsonify("CODIFY!"), 200
+def dados_homepage():
+    aluno = Aluno()
+    curso = Curso()
+    empresa = Empresa()
+    vaga = Vaga()
+    
+    n_alunos = aluno.count_ativos() + aluno.count_inativos()
+    n_cursos = curso.get_numero_cursos()
+    n_empresas = empresa.get_numero_empresas()
+    n_vagas = vaga.get_numero_vagas()
+    n_alunos_ativos = aluno.count_ativos()
+    n_alunos_inativos = aluno.count_inativos()
+
+    
+    dados_homepage = {
+        "num_alunos": n_alunos,
+        "num_cursos": n_cursos,
+        "num_empresas": n_empresas,
+        "num_vagas": n_vagas,
+        "num_alunos_ativos": n_alunos_ativos,
+        "num_alunos_inativos": n_alunos_inativos
+    }
+    return jsonify(dados_homepage), 200
 
 app.register_blueprint(aluno_blueprint)
 app.register_blueprint( curso_blueprint)
