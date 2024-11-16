e3b28fec8b85ec0a861c9e71146c0af185782dae
fonsecajohn <joaovictorfonseca@usp.br>
2024-11-16 18:25:26 -0300

    [Backend] tópico feito: Páginas cursos (a data agora está corrigida)

--- Backend/Database/classe_curso.py
+++ Backend/Database/classe_curso.py
@@ -5,23 +5,41 @@ class Curso():
         self.db = db_provider
     
     def get_cursos(self, nome: str = "", duracao: int = None, nivel: str = ""):
+        query = """
+        SELECT 
+            c.nome,
+            c.descricao,
+            c.duracao,
+            c.nivel,
+            TO_CHAR(c.data_lancamento, 'YYYY-MM-DD') AS data_lancamento,
+            COUNT(e.email_aluno) AS numero_alunos_concluidos
+        FROM 
+            curso c
+        LEFT JOIN 
+            estuda e ON c.nome = e.nome_curso AND e.data_conclusao IS NOT NULL
+        """
         
-        query = "SELECT * FROM curso"
         filtros = []
 
         if nome:
-            filtros.append(f"LOWER(nome) LIKE '%{nome.lower()}%'")
+            filtros.append(f"LOWER(c.nome) LIKE '%{nome.lower()}%'")
         
         if duracao is not None:
-            filtros.append(f"duracao = {duracao}")
+            filtros.append(f"c.duracao = {duracao}")
         
         if nivel:
-            filtros.append(f"LOWER(nivel) = '{nivel.lower()}'")
+            filtros.append(f"LOWER(c.nivel) = '{nivel.lower()}'")
         
         if filtros:
             query += " WHERE " + " AND ".join(filtros)
 
+        query += """
+        GROUP BY 
+            c.nome, c.descricao, c.duracao, c.nivel, c.data_lancamento
+        """
+    
         return self.db.execute_select_all(query)
+
 
     def get_numero_cursos(self) -> int:
         query = "SELECT COUNT(*) FROM curso"
