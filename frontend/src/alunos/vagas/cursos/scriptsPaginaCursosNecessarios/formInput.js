78d72319812bcf3b56b8b66f7b97dfa1f1413792
Thiago Kenji <thiago.kenji123@gmail.com>
2024-11-23 14:01:44 -0300

    [frontend] Feita a integração da paginaCursosNecessarios.

--- frontend/src/alunos/vagas/cursos/scriptsPaginaCursosNecessarios/formInput.js
+++ frontend/src/alunos/vagas/cursos/scriptsPaginaCursosNecessarios/formInput.js
@@ -1,7 +1,10 @@
 export function inputFunctionality(urlParams) {
-  const inputName = document.querySelector("form input[name='nome']")
+  const inputName = document.querySelector("form input[name='curso_nome']")
+  const selectLevel = document.querySelector("form select[name='nivel']")
 
-  const nameC = urlParams.get("nome")
+  const nameC = urlParams.get("curso_nome")
+  const level = urlParams.get("nivel")
 
   inputName.value = nameC !== null ? nameC : ""
+  selectLevel.value = level !== null ? level : ""
 }
